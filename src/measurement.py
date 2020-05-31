7aec9cfdb61130218983b9d5eeef480257fec8c1
Alexander Chambers <alexander.chambers8472@gmail.com>
2020-05-31 22:02:22 +0930

    Use black to format source

--- src/measurement.py
+++ src/measurement.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Measurement:
     """
     Form a number with an uncertainty.
@@ -15,8 +16,7 @@ class Measurement:
 
     """
 
-
-    def __init__(self,value,error):
+    def __init__(self, value, error):
         """Initialise the nominal value and the uncertainty
         of the Measurement
 
@@ -30,8 +30,7 @@ class Measurement:
     def __eq__(self, other):
         return (self.value == other.value) and (self.error == other.error)
 
-
-    def __add__(self,other):
+    def __add__(self, other):
         """
         Add two Measurements together
 
@@ -44,10 +43,10 @@ class Measurement:
                      Return uncertainty in quadrature.
         """
         total_value = self.value + other.value
-        total_error = math.sqrt(self.error**2 + other.error**2)
-        return Measurement(total_value,total_error )
+        total_error = math.sqrt(self.error ** 2 + other.error ** 2)
+        return Measurement(total_value, total_error)
 
-    def __sub__(self,other):
+    def __sub__(self, other):
         """
         Subtract two Measurements
 
@@ -61,11 +60,11 @@ class Measurement:
         """
 
         total_value = self.value - other.value
-        total_error = math.sqrt(self.error**2 + other.error**2)
+        total_error = math.sqrt(self.error ** 2 + other.error ** 2)
 
-        return Measurement(total_value,total_error)
+        return Measurement(total_value, total_error)
 
-    def __mul__(self,other):
+    def __mul__(self, other):
         """
         Multiply two Measurements together
 
@@ -78,12 +77,15 @@ class Measurement:
                      Return sqrt( nom2**2*unc1**2 + nom1**2*unc2**2).
         """
 
-        total_value = self.value*other.value
-        total_error = math.sqrt((other.value**2)*(self.error**2) + (self.value**2)*(other.error**2))
+        total_value = self.value * other.value
+        total_error = math.sqrt(
+            (other.value ** 2) * (self.error ** 2)
+            + (self.value ** 2) * (other.error ** 2)
+        )
 
-        return Measurement(total_value,total_error )
+        return Measurement(total_value, total_error)
 
-    def __truediv__(self,other):
+    def __truediv__(self, other):
         """
         Divide two Measurements together
 
@@ -96,19 +98,22 @@ class Measurement:
                      Return uncertainty propogated for a/b.
         """
 
-        if other.value == 0.0: raise ZeroDivisionError("Division by Zero error!")
+        if other.value == 0.0:
+            raise ZeroDivisionError("Division by Zero error!")
 
-        total_value = self.value/other.value
-        total_error = math.sqrt(( (1/other.value)**2)*(self.error**2) + (self.value/other.value/other.value)**2*(other.error**2))
-        return Measurement(total_value,total_error)
+        total_value = self.value / other.value
+        total_error = math.sqrt(
+            ((1 / other.value) ** 2) * (self.error ** 2)
+            + (self.value / other.value / other.value) ** 2 * (other.error ** 2)
+        )
+        return Measurement(total_value, total_error)
 
     def __repr__(self):
         """
         Return representation of Measurement
         Returns: value \u00B1 error
         """
-        return str(self.value) +"\u00B1" + str(self.error)
-
+        return str(self.value) + "\u00B1" + str(self.error)
 
 
 def main():
@@ -116,12 +121,12 @@ def main():
     Demonstrate the functionality of the Measurement class.
 
     """
-    x = Measurement(1,1)
-    y = Measurement(2,2)
-    z = Measurement(3,3)
+    x = Measurement(1, 1)
+    y = Measurement(2, 2)
+    z = Measurement(3, 3)
 
     print("===============================")
-    #help(Measurement)
+    # help(Measurement)
     print("===============================")
     print("x")
     print(x)
@@ -132,17 +137,17 @@ def main():
     print("===============================")
 
     print("addition")
-    print(x+y)
+    print(x + y)
     print("subtract")
-    print(x-y)
+    print(x - y)
     print("multiplication")
-    print(x*y)
+    print(x * y)
     print("division")
-    print(x/y)
+    print(x / y)
     print("===============================")
 
     print("x/y*z")
-    print(x/y*z)
+    print(x / y * z)
     print("===============================")
 
 
